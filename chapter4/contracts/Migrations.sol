d19e8f2a43977cda8e960008c7f40b50d2d04405
wsnevan <452365641@qq.com>
2022-02-16 13:47:27 +0800

    dapp first commit

--- chapter4/contracts/Migrations.sol
+++ chapter4/contracts/Migrations.sol
@@ -1,23 +1,25 @@
-pragma solidity ^0.4.17;
+pragma solidity >=0.4.0 <0.9.0;
 
 contract Migrations {
-  address public owner;
-  uint public last_completed_migration;
+    address public owner;
+    uint256 public last_completed_migration;
 
-  modifier restricted() {
-    if (msg.sender == owner) _;
-  }
+    modifier restricted() {
+        if (msg.sender == owner) _;
+    }
 
-  function Migrations() public {
-    owner = msg.sender;
-  }
+    function Migrations1() public {
+        owner = msg.sender;
+    }
 
-  function setCompleted(uint completed) public restricted {
-    last_completed_migration = completed;
-  }
+    function setCompleted(uint256 completed) public restricted {
+        last_completed_migration = completed;
+    }
 
-  function upgrade(address new_address) public restricted {
-    Migrations upgraded = Migrations(new_address);
-    upgraded.setCompleted(last_completed_migration);
-  }
+    function upgrade(address new_address) public restricted {
+        Migrations upgraded = Migrations(new_address);
+        upgraded.setCompleted(last_completed_migration);
+    }
 }
+
+//this is git testing massage
--- chapter4/contracts/Voting.sol
+++ chapter4/contracts/Voting.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.18;
+pragma solidity >=0.4.0 <0.9.0;
 import "./ECRecovery.sol";
 
 contract Voting {
